68262769f3bbef5295e0087b0bbbf5afff6b14f8
Minhnhatz <166145022+Minhnhatz@users.noreply.github.com>
2025-09-26 21:39:51 +0700

    Update redzz_full_beautified.lua

--- redzz_full_beautified.lua
+++ redzz_full_beautified.lua
@@ -1,5 +1,4 @@
 local MarketplaceService = game:GetService("MarketplaceService")
-local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
 local HttpService = game:GetService("HttpService")
 local RunService = game:GetService("RunService")
